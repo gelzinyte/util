31bf35941e5dcb4d952b3e1d14a54dd53d61e448
gelzinyte <elena.gelzinyte@gmail.com>
2021-04-27 14:56:38 +0100

    dft reevaluation function too

--- util/bde/__init__.py
+++ util/bde/__init__.py
@@ -7,6 +7,7 @@ from wfl.generate_configs import minim
 from wfl.configset import ConfigSet_in, ConfigSet_out
 from wfl.calculators import generic
 from wfl.calculators import orca
+from wfl.pipeline import iterable_loop
 
 from util import smiles  # TODO change to wfl smiles once merged
 from util import radicals  # TODO change to wfl once merged
@@ -26,34 +27,20 @@ def evaluate_gap_on_h(gap_filename, gap_prop_prefix, output_filename):
     return H
 
 
-def gap_prepare_bde_structures_multiple(smiles, gap_filename, gap_prop_prefix,
-                                        etc):
+def gap_prepare_bde_structures_parallel(smiles, outputs, calculator,
+                                        gap_prop_prefix, chunksize=1):
     """using iterable_loop"""
-    pass
+    return iterable_loop(iterable=smiles, configset_out=outputs,
+                         op=gap_prepare_bde_structures, chunksize=chunksize)
+    
 
-
-
-def gap_optimise(atoms, calculator):
-
-    opt_kwargs = {'logfile':'log.txt', 'master':True, 'precon':None,
-                    'use_armijo':False}
-
-    atoms_opt_traj = minim.run_op(atoms, calculator, keep_symmetry=False,
-                             update_config_type=False, **opt_kwargs)
-
-    atoms_opt = [traj[-1] for traj in atoms_opt_traj]
-
-    return atoms_opt
-
-
-def gap_prepare_bde_structures_single(smiles, name, calculator, gap_prop_prefix):
+def gap_prepare_bde_structures(smiles, name, calculator, gap_prop_prefix):
     """takes in single smiles, makes 3D structure, optimises with GAP,
     removes sp3 hydrogens, optimises with GAP"""
 
     # make molecule
     mol = smiles.run_op(smiles)[0]
     mol.info['config_type'] = name
-    # mol = process_info_labels(mol, name, 'mol')
 
     # gap-optimise
     mol = gap_optimise(mol, calculator)
@@ -108,4 +95,39 @@ def gap_prepare_bde_structures_single(smiles, name, calculator, gap_prop_prefix)
 def dft_reoptimise(inputs, outputs, dft_prefix):
     """reoptimises all the structures given and puts them in appropriate
     atoms.info/arrays entries"""
-    pass
+
+    default_kw = Config.from_yaml(
+        os.path.join(cfg['util_root'], 'default_kwargs.yml'))
+    smearing = default_kw['orca']['smearing']
+    orcasimpleinput = default_kw['orca']['orcasimpleinput']
+    orcablocks = f'%scf Convergence Tight\nSmearTemp {smearing}\nmaxiter 500\nend'
+
+
+    orca_kwargs = {'orcasimpleinput': orcasimpleinput,
+                   'orcablocks': orcablocks,
+                   'task':'opt'}
+
+    output_prefix = f'{dft_prefix}_opt_'
+
+    orca.evaluate(inputs, outputs, base_rundir='orca_opt_outputs',
+                  orca_kwargs=orca_kwargs, output_prefix=output_prefix)
+
+    atoms_out = []
+    for at in outputs.to_ConfigSet_in():
+        at.arrays[f'{dft_prefix}_opt_positions'] = at.positions.copy()
+        atoms_out.append(at)
+
+    return atoms_out
+
+
+def gap_optimise(atoms, calculator):
+
+    opt_kwargs = {'logfile':'log.txt', 'master':True, 'precon':None,
+                    'use_armijo':False}
+
+    atoms_opt_traj = minim.run_op(atoms, calculator, keep_symmetry=False,
+                             update_config_type=False, **opt_kwargs)
+
+    atoms_opt = [traj[-1] for traj in atoms_opt_traj]
+
+    return atoms_opt
