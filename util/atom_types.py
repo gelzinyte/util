4da30190bbeea63a2f88195d0803beac8c016389
gelzinyte <elena.gelzinyte@gmail.com>
2021-06-17 10:23:05 +0100

    making atom typed atoms

--- util/atom_types.py
+++ util/atom_types.py
@@ -1,6 +1,8 @@
+import os
 from ase import neighborlist
 import numpy as np
 import warnings
+import yaml
 
 
 def assign_aromatic(inputs, outputs, elements_to_type, mult=1):
@@ -98,6 +100,71 @@ def assign_aromatic(inputs, outputs, elements_to_type, mult=1):
 
     print(f'bad_config idx ({len(bad_configs)}):', bad_configs)
     outputs.end_write()
+
+def prepare_empty_atom_type_lookup_yamls(
+        atom_type_dir='atom_type_lookup_fresh'):
+
+    all_atoms = read('original_datasets.xyz', ':')
+
+    config_type_numbers = {}
+    for idx, at in enumerate(all_atoms):
+        config_type = at.info['config_type']
+        numbers = at.numbers
+        if config_type not in config_type_numbers.keys():
+            config_type_numbers[config_type] = numbers
+        else:
+            if not np.all(config_type_numbers[config_type] == numbers):
+                raise RuntimeError('some configs have their atoms reordered')
+
+    for config_type, numbers in config_type_numbers.items():
+
+        # data = {config_type: yaml_data(numbers)}
+        string = f'{config_type}:\n' + yaml_string(numbers)
+        with open(os.path.join(atom_type_dir, config_type + '.yml'), 'w') as \
+                file:
+            file.write(string)
+
+def yaml_string(numbers):
+    string = ''
+    for number in numbers:
+        string += f'- {number}: {number}\n'
+    return string
+
+
+def yaml_data(numbers):
+    yaml_list = []
+    for number in numbers:
+        yaml_list.append({str(number) : str(number)})
+    return yaml_list
+
+def atom_type(at, atom_type_dir='atom_type_lookup'):
+
+    config_type = at.info['config_type']
+    orig_numbers, atom_typed_numbers = numbers_from_yaml(os.path.join(
+        atom_type_dir, config_type+'.yml'), config_type)
+
+    at.arrays['original_numbers'] = orig_numbers
+    at.arrays['atom_typed_numbers'] = atom_typed_numbers
+    at.numbers = atom_typed_numbers
+
+    return at
+
+def numbers_from_yaml(filename, config_type):
+    with open(str(filename)) as yaml_file:
+        data = yaml.safe_load(yaml_file)[config_type]
+
+    orig_numbers = []
+    atom_typed_numbers = []
+    for num_pair in data:
+        for key, val in num_pair.items():
+            orig_numbers.append(key)
+            atom_typed_numbers.append(val)
+
+    return orig_numbers, atom_typed_numbers
+
+
+
+
 
 
 
