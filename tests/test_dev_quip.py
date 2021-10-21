f7904a61da3479d0ad82e3c2cb89f10688694ed7
gelzinyte <elena.gelzinyte@gmail.com>
2021-10-21 10:30:41 +0100

    updated tests

--- tests/test_dev_quip.py
+++ tests/test_dev_quip.py
@@ -3,6 +3,7 @@ import pytest
 from ase.io import read, write
 import numpy as np
 import os
+from quippy.descriptors import Descriptor
 
 def ref_path():
     return os.path.abspath(os.path.dirname(__file__))
@@ -27,15 +28,32 @@ def do_desc(quip_version='modified'):
     if quip_version == 'modified':
         quip = '/home/eg475/dev/dev_QUIP/build/linux_x86_64_gfortran_openmp' \
                '/quip'
+        # quip = '/home/eg475/dev/dev_QUIP/build/linux_x86_64_gfortran' \
+        #            '/quip'
+
+        command = f" {quip} atoms_filename=tmp.xyz " \
+                  f'descriptor_str={{soap l_max=6 n_max=12 cutoff=3 delta=1 ' \
+                  f'' \
+                  f'covariance_type=dot_product zeta=4 ' \
+                  f'n_sparse=100 sparse_method=cur_points ' \
+                  f'atom_gaussian_width=0.3 cutoff_transition_width=0.5  ' \
+                  f'add_species=True }} ' \
+                  f'calc_args={{atom_gaussian_weight_name' \
+                  f'=atom_gaussian_weight}} > ' \
+                  f'desc.out'
+
     elif quip_version == 'original':
         quip = '/opt/womble/QUIP/2021_07_06//bin/quip'
 
-    command = f" {quip} atoms_filename=tmp.xyz " \
-              f'descriptor_str={{soap l_max=6 n_max=12 cutoff=3 delta=1 ' \
-              f'covariance_type=dot_product zeta=4 ' \
-              f'n_sparse=100 sparse_method=cur_points ' \
-              f'atom_gaussian_width=0.3 cutoff_transition_width=0.5  ' \
-              f'add_species=True}} > desc.out'
+        command = f" {quip} atoms_filename=tmp.xyz " \
+                  f'descriptor_str={{soap l_max=6 n_max=12 cutoff=3 delta=1 ' \
+                  f'covariance_type=dot_product zeta=4 ' \
+                  f'n_sparse=100 sparse_method=cur_points ' \
+                  f'atom_gaussian_width=0.3 cutoff_transition_width=0.5  ' \
+                  f'add_species=True }} > desc.out'
+
+    assert os.path.isfile(quip)
+
 
     print(command)
 
@@ -57,7 +75,7 @@ def parse_output(output_fname):
 
 
 def prep_at(at, q_list, temp_fname):
-    at.arrays["at_gaussian_weight"] = q_list
+    at.arrays["atom_gaussian_weight"] = q_list
     write(temp_fname, at)
 
 def test_modified_quip_same_as_not(at_in):
@@ -83,7 +101,20 @@ def test_downweigting_single_gaussian_changes_descriptor(at_in):
 
 
 
+def test_calculate_descriptor(at_in):
 
+    modified_quip_desc_halves = calc_desc(at_in,
+                                          np.array([1.0, 1.0, 1.0, 0.5]),
+                                          quip_version='modified')
+
+    at_in.arrays["atom_gaussian_weight"] = np.array([1.0, 1.0, 1.0, 0.5])
+    d = Descriptor(args_str="soap l_max=6 n_max=12 cutoff=3 delta=1 covariance_type=dot_product zeta=4 "\
+                   "n_sparse=100 sparse_method=cur_points atom_gaussian_width=0.3 cutoff_transition_width=0.5  "\
+                   "add_species=True", ase_to_quip_kwargs={'add_arrays':"atom_gaussian_weight"})
+    my_desc = d.calc(at_in,
+                     args_str="atom_gaussian_weight_name=atom_gaussian_weight")
+
+    assert pytest.approx(my_desc['data']) == modified_quip_desc_halves
 
 
 
