9e2cd80b3be73d22740ee663470696a0e6d2daa6
gelzinyte <elena.gelzinyte@gmail.com>
2022-05-17 11:50:06 +0100

    bug nd move atom_types out

--- tests/test_calc_descriptor.py
+++ tests/test_calc_descriptor.py
@@ -6,7 +6,7 @@ from util.configs import calc_desc
 
 
 def test_calculate_descriptor():
-    param_fname = Path().resolve() / 'test_gap_iter_fit' / 'parameters.yml'
+    param_fname = Path().resolve() / 'tests' / 'test_gap_iter_fit' / 'parameters.yml'
     param_fname = param_fname.as_posix()
     atoms = molecule('CH4')
     calc_desc.from_param_yaml(atoms, param_fname)
