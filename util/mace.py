02a8a31ad9b2b70cb3ee75cebeff3bde08dd95d0
gelzinyte <elena.gelzinyte@gmail.com>
2023-01-26 10:38:36 +0000

    updated mace descriptor script to skip failures

--- util/mace.py
+++ util/mace.py
@@ -2,6 +2,7 @@ import numpy as np
 from wfl.utils.parallel import construct_calculator_picklesafe
 from ase import Atoms
 from wfl.autoparallelize import autoparallelize
+from ase.io import read, write
 
 
 def to_cpu(model_path_in, model_path_out):
@@ -11,29 +12,50 @@ def to_cpu(model_path_in, model_path_out):
     torch.save(model, model_path_out)
 
 
-def calc_descriptor_ll(atoms, calculator, prefix='mace_'):
+def calc_descriptor_ll(atoms, calculator, prefix='mace_', normalize=True, skip_unnormalizable=True):
 
     calculator = construct_calculator_picklesafe(calculator)    
 
     if isinstance(atoms, Atoms):
         atoms = [atoms]
 
+    ats_out = []
     for at in atoms:
+        at = at.copy()
 
         calculator.calculate(at)
 
         local_desc = calculator.extra_results["descriptor"]
-        local_desc = local_desc / np.linalg.norm(local_desc, axis=1).reshape(len(local_desc), 1)
-
         global_sum = np.sum(local_desc, axis=0)
-        global_sum = global_sum / np.linalg.norm(global_sum)
-
         global_mean = np.mean(local_desc, axis=0)
-        global_mean= global_mean / np.linalg.norm(global_mean)
 
-        at.arrays[f"{prefix}local_desc"] = local_desc
+        if normalize:
+
+            local_norm = np.linalg.norm(local_desc, axis=1)
+
+            if np.any(local_norm == 0):
+                write(f"{at.info['dataset_type']}_bad_descriptor.xyz", at)
+
+                if skip_unnormalizable:
+                    local_desc = None
+                else:
+                    raise RuntimeError("found bad descriptor")
+
+            if local_desc is not None:
+                local_desc = local_desc / local_norm.reshape(len(local_desc), 1)
+
+            global_sum = global_sum / np.linalg.norm(global_sum)
+            global_mean= global_mean / np.linalg.norm(global_mean)
+
+        if local_desc is not None:
+            at.arrays[f"{prefix}local_desc"] = local_desc
+        else:
+            at.info[f"{prefix}local_desc_failed"] = "FAILED_DESCRIPTOR"
+
         at.info[f"{prefix}global_sum_desc"] = global_sum
         at.info[f"{prefix}global_mean_mace_desc"] = global_mean
+
+        ats_out.append(at)
     
     return atoms 
 
